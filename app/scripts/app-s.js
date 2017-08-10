c24598302dbcd5dee2aac607d7a0c37e1baae29b
Bellatrix988 <marina.yurova1996@yandex.ru>
2017-08-10 13:38:23 +0300

    added comments

--- app/scripts/app-s.js
+++ app/scripts/app-s.js
@@ -1,5 +1,7 @@
+//common module include in self other child-module
 var app = angular.module('instaApp',['insta_apiApp', 'routingApp']);
- var app = angular.module('insta_apiApp',['ngDialog']);
+ var app = angular.module('insta_apiApp',[]);
+ //module for processing data from API
  angular.module('insta_apiApp')
  		.config(['$sceDelegateProvider','$qProvider', function($sceDelegateProvider, $qProvider) {
  			$qProvider.errorOnUnhandledRejections(false);
@@ -12,15 +14,18 @@ var app = angular.module('instaApp',['insta_apiApp', 'routingApp']);
 		    "https://www.instagram.com/**/*"
 		  ]);
  		}])
+        //service for getting data from url
  		.service('ApiData', ['$http', function($http){
  			this.getData = function(url){
 	                return $http.get(url);
  			};
  		}])
+        //factory for Authentication user
  		.factory('InstagramService', ['$window', function ( $window) {
 			    var client_id = "4a73bf00df514996b3ce157f0e804700";
  				var redirect_uri = "http://192.168.0.103:3000";//'https://bellatrix988.ru/content/instagram-client.html';//
 			    var service = {
+                    //redirects user on page-Authentication
 			        login: function () {
 			            $window.open("https://instagram.com/oauth/authorize/?client_id=" + client_id +
 			                "&redirect_uri=" + redirect_uri +
@@ -29,47 +34,46 @@ var app = angular.module('instaApp',['insta_apiApp', 'routingApp']);
 			    };
 			    return service;
  		}])
-	    .controller("userData",['$scope', 'ApiData', 'ngDialog', '$location', function($scope, ApiData, ngDialog, $location){
-            $scope.clickToOpen = function (token,id) {
-            	$scope.getMedia(token, id);
-		        ngDialog.open({
-		         template: '../../html-part/post.html', 
-		         className: 'ngdialog-theme-default' 
-		     });
-		    };
-
+	    .controller("userData",['$scope', 'ApiData', '$location', function($scope, ApiData, $location){
+            //function from service
             getData = function(url){
             	return ApiData.getData(url);
             }
+
             $scope.token = '-1';
             $scope.media_id = '-1';
-            //get info about user with current token
-            var url = $location.absUrl();
-            if(url.includes('token=')){
-                var index = url.lastIndexOf('n=');
-                var indEnd = url.lastIndexOf('&id');
-                var token = url.substring(index+2,51 + index + 2);
-                console.log('TOKEN = ', token);
-                $scope.token = token;
-            }
-            if(url.includes('id')){
-                var index = url.lastIndexOf('=');
-                var media_id = url.substring(index+1);
-                console.log('media_id = ', media_id);
-                $scope.media_id = media_id;
-            }
+
+            //region get info about user with current token and id
+                var url = $location.absUrl();
+                if(url.includes('token=')){
+                    var index = url.lastIndexOf('n=');
+                    var indEnd = url.lastIndexOf('&id');
+                    var token = url.substring(index+2,51 + index + 2);
+                    console.log('TOKEN = ', token);
+                    $scope.token = token;
+                }
+                if(url.includes('id')){
+                    var index = url.lastIndexOf('=');
+                    var media_id = url.substring(index+1);
+                    console.log('media_id = ', media_id);
+                    $scope.media_id = media_id;
+                }
+            //endregion
+
             if($scope.token !== '-1'){
  				var url = 'https://api.instagram.com/v1/users/self/?access_token='+ $scope.token;
                 var urlMediaInfo = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + $scope.token;
-
+                //get data about user
                 getData(url).then(function(response){
 					$scope.data = response.data.data;
 	            });
+                //get last loaded media data
 	            getData(urlMediaInfo).then(function(response){
 	            	$scope.medias = response.data.data;
 	            });
             }
 
+            //get info and comments about media data with current id
             $scope.getMedia = function(token, id){
                 var urlMediaInfo = 'https://api.instagram.com/v1/media/'+id+'?access_token=' + token;
                 var urlMediaComments = 'https://api.instagram.com/v1/media/'+ id +'/comments?access_token='+ token;
--- app/scripts/app/app.js
+++ app/scripts/app/app.js
@@ -1,1 +1,2 @@
+//common module include in self other child-module
 var app = angular.module('instaApp',['insta_apiApp', 'routingApp']);
